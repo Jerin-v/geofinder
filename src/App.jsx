94471e427e9cbaf43f631b0559cb076470492794
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-28 14:39:02 -0500

    listed all countries and their flags

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,35 @@ import { useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
+  const [countries, setCountries] = useState(null)
 
   useEffect(() => {
     const fetchCountryData = async () => {
       const response = await fetch("https://restcountries.com/v3.1/all")
       const json = await response.json()
-      console.log(json[100])
+      setCountries(json)
+      console.log(json)
     }
 
     fetchCountryData()
-  })
+  },[])
 
   return (
     <div>
+      <h1>Geo Finder</h1>
+      <input
+        type='text'
+        placeholder='Search...'
+      />
+      <ul>
+        {countries && countries.map(country => 
+          <li key={country.cca2}>
+            <img src={country.flags.png} />
+            {country.name.common}
+          </li>
+
+        )}
+      </ul>
 
     </div>
   )
