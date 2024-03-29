06275907d8a25f3da65800dc4ff36c4312f46215
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-29 13:52:03 -0500

    added summary statistics

--- src/App.jsx
+++ src/App.jsx
@@ -46,19 +46,37 @@ function App() {
     setFiltered(filteredData)
   }
 
+  //To do 
+  //add summary statistics containers [x]
+  //summary statistics avg pop, # of landlocked countries, common language [x]
+  //add filter buttons pop(slider?), continent, region, 
+  //convert list to table format
+  //add sidebar
+
   return (
-    <div>
+    <div className='wholePage'>
       <h1>Geo Finder</h1>
+      <div className='summaryStats'>
+        <div className='statCard'>
+          <h2>Largest Region</h2>
+          <p>Asia</p>
+        </div>
+        <div className='statCard'>
+          <h2>Most Common Language</h2>
+          <p>Chinese</p>
+        </div>
+        <div className='statCard'>
+          <h2>Landlocked Countries</h2>
+          <p>44</p>
+        </div>
+
+      </div>
       <input
         type='text'
         placeholder='Search...'
         onChange={(input) => searchItems(input.target.value)}
       />
       <div className='listBox'>
-        {
-          //change list into table format
-          //move to another component if it gets too big
-        }
         <ul>
           {search.length > 0 ?
           (filtered.map(country => 
