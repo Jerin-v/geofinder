061ddd2a935d526fdc19cd273145b1fda457f319
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-04 14:46:12 -0500

    Add CountryDetail route to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './routes/Layout.jsx'
+import CountryDetail from './CountryDetail.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index={true} path="/" element={<App />} />
-          </Route>
+          <Route index={false} path='/countryDetails/:code' element={<CountryDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
